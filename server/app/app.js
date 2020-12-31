412178ef05fcf69acfc82bae21470108b525333f
Logan Thirion <thirionlogan@gmail.com>
2020-12-30 20:19:06 -0600

    #29 implement error handler

--- server/app/app.js
+++ server/app/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const errorHandler = require('../middleware/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(errorHandler);
 
 const getSomething = () => {
   return Promise.resolve('something');
