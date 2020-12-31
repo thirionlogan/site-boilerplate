a4e5e4f62dd87bb3737d4f2e269fdecda5a96216
Logan Thirion <thirionlogan@gmail.com>
2020-12-30 20:11:59 -0600

    #26 create server

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -2,6 +2,6 @@ import { rest } from 'msw';
 
 export const handlers = [
   rest.get('http://localhost:3001/', (req, res, ctx) => {
-    return res(ctx.json({}), ctx.status(200));
+    return res(ctx.json({ message: 'something' }), ctx.status(200));
   }),
 ];
