183c4ff344962bd56f0de0f360651400e266afbd
Logan Thirion <thirionlogan@gmail.com>
2021-01-10 02:11:46 -0600

    #60 Implement data store

--- server/data/seeds/004_seed_user.js
+++ server/data/seeds/004_seed_user.js
@@ -1,28 +1,15 @@
 exports.seed = (knex) => {
-  // Deletes ALL existing entries
   return knex('user')
     .del()
     .then(() => {
-      // Inserts seed entries
-      return knex('user')
-        .insert([
-          {
-            first_name: 'John',
-            last_name: 'Doe',
-            email: 'johndoe@email.com',
-            password:
-              '$2a$10$YSPUaf2rbvPwyxROi7zOaeEQQUM6G1m.1K3dkoJzrGYlAIlilnEre',
-          },
-        ])
-        .then(() => {
-          return knex('auth_token').insert([
-            {
-              token:
-                'd77f942c51f5f424594c5630be3146f7ac92efe6d9e6b86e63aed4454a05',
-              user_id: 1,
-            },
-          ]);
-        });
+      return knex('user').insert([
+        {
+          first_name: 'John',
+          last_name: 'Doe',
+          email: 'johndoe@email.com',
+          password:
+            '$2a$10$YSPUaf2rbvPwyxROi7zOaeEQQUM6G1m.1K3dkoJzrGYlAIlilnEre',
+        },
+      ]);
     });
 };
-//XohImNooBHFR0OVvjcYpJ3NgPQ1qq73WKhHvch0VQtg=
