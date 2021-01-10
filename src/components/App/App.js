9f1491493c08671ad79fa74d83b0136822151765
Logan Thirion <thirionlogan@gmail.com>
2021-01-10 01:12:29 -0600

    #52 Login Front End

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,24 @@
-import './App.css';
 import React from 'react';
+import LoginPage from '../LoginPage/LoginPage';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { logInClient } from '../../client/client';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className='App'>
+        <LoginPage logInClient={logInClient} />
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default App;
