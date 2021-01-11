78ef1ed274d7fe55c2fe3926710e5e2d2d6b24b8
Logan Thirion <thirionlogan@gmail.com>
2021-01-10 23:33:01 -0600

    #51 implement Register Page

---
+++ src/components/RegisterPage/RegisterPage.js
@@ -0,0 +1,196 @@
+import React, { useState } from 'react';
+import {
+  Avatar,
+  Button,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+} from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+import { Link as RouterLink, Redirect } from 'react-router-dom';
+import Copyright from '../Copyright/Copyright';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function RegisterPage({ registerUser }) {
+  const classes = useStyles();
+
+  const [email, setEmail] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+
+  const handleRedirect = () => {
+    setRedirect(true);
+  };
+
+  const handleChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleChangeLastName = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleChangeFirstName = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleChangeConfirmPassword = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleRegisterUser = (event) => {
+    registerUser({
+      email,
+      firstName,
+      lastName,
+      password,
+      confirmPassword,
+    })
+      .then((res) => {
+        if (res.status === 201) {
+          handleRedirect();
+        }
+      })
+      .catch((err) => {
+        //TODO handle error
+      });
+  };
+
+  return (
+    <Container component='main' maxWidth='xs'>
+      {redirect ? <Redirect to='/login' /> : null}
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign up
+        </Typography>
+        <form className={classes.form} onSubmit={(e) => e.preventDefault()}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete='fname'
+                name='firstName'
+                variant='outlined'
+                required
+                fullWidth
+                id='firstName'
+                label='First Name'
+                autoFocus
+                onChange={handleChangeFirstName}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                id='lastName'
+                label='Last Name'
+                name='lastName'
+                autoComplete='lname'
+                onChange={handleChangeLastName}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                id='email'
+                label='Email Address'
+                name='email'
+                autoComplete='email'
+                onChange={handleChangeEmail}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                name='password'
+                label='Password'
+                type='password'
+                id='password'
+                autoComplete='current-password'
+                onChange={handleChangePassword}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                name='confirmPassword'
+                label='Confirm Password'
+                type='password'
+                id='confirmPassword'
+                onChange={handleChangeConfirmPassword}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value='allowExtraEmails' color='primary' />}
+                label='I want to receive inspiration, marketing promotions and updates via email.'
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+            onClick={handleRegisterUser}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify='flex-end'>
+            <Grid item>
+              <Link to='/login' component={RouterLink} variant='body2'>
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
