efac00f23d13b5fecc1c5fafc16836baf25f96c4
Logan Thirion <thirionlogan@gmail.com>
2021-01-10 23:37:25 -0600

    Route between pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import LoginPage from '../LoginPage/LoginPage';
+import React, { useState } from 'react';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { logInClient } from '../../client/client';
+import { logInClient, registerUser, logOutClient } from '../../client/client';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link as RouterLink,
+} from 'react-router-dom';
+import LoginPage from '../LoginPage/LoginPage';
+import HomePage from '../HomePage/HomePage';
+import RegisterPage from '../RegisterPage/RegisterPage';
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  MenuItem,
+  Menu,
+} from '@material-ui/core';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuIcon from '@material-ui/icons/Menu';
+import { makeStyles } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   palette: {
@@ -10,13 +30,121 @@ const theme = createMuiTheme({
   },
 });
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
 const App = () => {
+  const classes = useStyles();
+  const [user, setUser] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogin = (user) => {
+    setUser(user);
+  };
+
+  const handleLogout = () => {
+    logOutClient();
+    setUser(false);
+    handleClose();
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div className='App'>
-        <LoginPage logInClient={logInClient} />
-      </div>
+      <Router>
+        <div className='App'>
+          <AppBar position='static'>
+            <Toolbar>
+              <IconButton
+                edge='start'
+                className={classes.menuButton}
+                color='inherit'
+                aria-label='menu'
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant='h6' className={classes.title}>
+                Site Boilerplate
+              </Typography>
+              {user ? (
+                <div>
+                  <IconButton
+                    aria-label='account of current user'
+                    aria-controls='menu-appbar'
+                    aria-haspopup='true'
+                    onClick={handleMenu}
+                    color='inherit'
+                  >
+                    <AccountCircle />
+                  </IconButton>
+                  <Menu
+                    id='menu-appbar'
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={handleClose}
+                  >
+                    <MenuItem onClick={handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem
+                      onClick={handleLogout}
+                      to='/login'
+                      component={RouterLink}
+                    >
+                      Log out
+                    </MenuItem>
+                  </Menu>
+                </div>
+              ) : (
+                <Button color='inherit' to='/login' component={RouterLink}>
+                  Login
+                </Button>
+              )}
+            </Toolbar>
+          </AppBar>
+          <Switch>
+            <Route exact path='/'>
+              <HomePage />
+            </Route>
+            <Route exact path='/login'>
+              <LoginPage
+                logInClient={logInClient}
+                handleLogin={handleLogin}
+                user={user}
+              />
+            </Route>
+            <Route exact path='/register'>
+              <RegisterPage registerUser={registerUser} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 };
