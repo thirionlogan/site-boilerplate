0e84c89d22a63087b7cadf34cdaeef20e414291a
Logan Thirion <thirionlogan@gmail.com>
2021-01-10 21:38:20 -0600

    implement Copyright

---
+++ src/components/Copyright/Copyright.test.js
@@ -0,0 +1,22 @@
+import Copyright from './Copyright';
+import { MemoryRouter } from 'react-router-dom';
+import { Link, Typography } from '@material-ui/core';
+import { mount } from 'enzyme';
+import React from 'react';
+
+describe('Copyright', () => {
+  let component;
+  beforeEach(() => {
+    component = mount(
+      <MemoryRouter>
+        <Copyright />
+      </MemoryRouter>
+    );
+  });
+  it('should render', () => {
+    expect(component.find(Link).props().to).toBe('/');
+    expect(component.find(Typography).first().text()).toContain(
+      'Copyright © Site Boilerplate'
+    );
+  });
+});
