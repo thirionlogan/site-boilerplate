dcc0731e4a5025e9ce7be56ce1654d657e7f825f
Logan Thirion <thirionlogan@gmail.com>
2021-01-11 11:16:15 -0600

    eslint

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -4,5 +4,5 @@ import App from './App';
 
 it('renders learn react link', () => {
   const component = shallow(<App />);
-  expect(true).toBe(true);
+  expect(component.exists('div')).toBe(true);
 });
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -2,8 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
